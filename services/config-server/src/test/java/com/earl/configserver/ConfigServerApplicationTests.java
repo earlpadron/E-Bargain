57e6b324121d8f7e8f36f59cf54c409871371557
Earl Padron <earlpadron24@gmail.com>
2024-07-27 14:08:08 -0700

    fixed missing config server annotation

---
+++ services/config-server/src/test/java/com/earl/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earl.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
