59371340550397099625b1e63b0ce9ae2371e23f
Earl Padron <earlpadron24@gmail.com>
2024-07-28 23:58:54 -0700

    attempt to use mysql instead of postgres

---
+++ services/product/src/main/java/com/earl/ecommerce/category/Category.java
@@ -0,0 +1,26 @@
+package com.earl.ecommerce.category;
+
+import com.earl.ecommerce.product.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
---
+++ services/product/src/main/java/com/earl/ecommerce/product/Product.java
@@ -0,0 +1,31 @@
+package com.earl.ecommerce.product;
+
+import com.earl.ecommerce.category.Category;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private Double availableQuantity;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
