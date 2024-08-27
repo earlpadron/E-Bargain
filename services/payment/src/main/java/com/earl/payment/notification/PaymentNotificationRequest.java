ac85701aed7c946225ae0c113696f7c5c9a92045
Earl Padron <earlpadron24@gmail.com>
2024-08-27 10:13:59 -0700

    finished payment service

---
+++ services/payment/src/main/java/com/earl/payment/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.earl.payment.notification;
+
+import com.earl.payment.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}
